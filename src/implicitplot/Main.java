3a1e2a46171964c4cb5c2f7528c529cba0c26a00
philipxjm <philipxjm1@gmail.com>
2014-05-31 02:20:03 -0400

    Made a start up screen, check it

--- src/implicitplot/Main.java
+++ src/implicitplot/Main.java
@@ -1,8 +1,42 @@
 package implicitplot;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		implicitplot.graphics.Client.run();
+		//implicitplot.graphics.Client.guiInit();
+		//Fields
+		JFrame login = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel w = new JLabel("Enter Width Here:");
+		JLabel h = new JLabel("Enter Height Here:");
+		JTextField width = new JTextField(6);
+		JTextField height = new JTextField(6);
+		JButton start = new JButton("Start");
+
+		//Methods
+		login.add(panel);
+		login.setVisible(true);
+        login.setBounds(100, 100, 365, 170);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setLocationRelativeTo(null);
+        login.setTitle("Implicit Plotter");
+
+        panel.setLayout(null);
+        Insets insets = panel.getInsets();
+        Dimension size1 = width.getPreferredSize();
+        Dimension size2 = w.getPreferredSize();
+        Dimension size3 = h.getPreferredSize();
+        panel.add(w);
+        panel.add(width);
+        panel.add(h);
+        panel.add(height);
+        panel.add(start);
+        width.setBounds(150 + insets.left, 30 + insets.top,size1.width, size1.height);
+        height.setBounds(150 + insets.left, 80 + insets.top,size1.width, size1.height);
+        w.setBounds(25 + insets.left, 30 + insets.top,size2.width, size2.height);
+        h.setBounds(25 + insets.left, 80 + insets.top,size3.width, size3.height);
+        start.setBounds(250,30,70,70);
 	}
 }
