e8ad046717d36f80912e87e924f6237d6d3c0155
DanielBoerlage <daniel.brlg@gmail.com>
2014-05-31 22:23:58 -0700

    added 34 more utility functions

--- src/implicitplot/Main.java
+++ src/implicitplot/Main.java
@@ -9,40 +9,8 @@ import implicitplot.equations.*;
 public class Main {
 
 	public static void main(String[] args) {
-		implicitplot.graphics.StartUpWindow.run();
-		//eqnTest();
-		Equation eqn = new Equation("mult(plus(1,x),plus(1,1,1),neg(y))))");  // (x+1)*(1+1+1)*(-y)
+		//implicitplot.graphics.StartUpWindow.run();
+		Equation eqn = new Equation("min(-1,-2,-1,-4)");  // (x+1)*(1+1+1)*(-y)
 		System.out.println(eqn.root.evaluate(3, 1/2.0));
-	}
-
-	public static void eqnTest() {
-		//this example string would be  x^2 - 1   or   minus(mult(x,x),1)
-		/*ParseTree tree = new Function(Function.Type.MINUS);
-		tree.children = new ParseTree[2];
-		tree.children[0] = new Function(Function.Type.MULT);
-		tree.children[0].children = new ParseTree[2];
-		tree.children[0].children[0] = new Variable(Variable.Type.X);
-		tree.children[0].children[1] = new Variable(Variable.Type.X);
-		tree.children[1] = new Constant(1d);*/
-
-		//ParseTree tree = new Variable(Variable.Type.X);
-
-		/*ParseTree tree = new Function(Function.Type.MINUS);
-		tree.addChild(new Function(Function.Type.MULT));
-		tree.addChild(new Constant(1d));
-		tree.children[0].addChild(new Variable(Variable.Type.X));
-		tree.children[0].addChild(new Variable(Variable.Type.X));*/
-
-		ParseTree tree = new Function(Function.Type.MINUS);
-		tree.addChild(new Function(Function.Type.MULT));
-		tree.addChild(new Constant(1d));
-		tree.children[0].addChild(new Variable(Variable.Type.X));
-		tree.children[0].addChild(new Variable(Variable.Type.X));
-
-		long startTime = System.currentTimeMillis();
-		double var = 0;
-		for(int i = 0; i < 100001; i++)
-			var = tree.evaluate(var, 0);
-		System.out.println("result: " + var + "  computed in " + (System.currentTimeMillis() - startTime) + " ms");
 	}
 }
--- src/implicitplot/equations/Function.java
+++ src/implicitplot/equations/Function.java
@@ -4,63 +4,155 @@ import java.util.Iterator;
 
 public class Function extends ParseTree {
     
-    public enum Type {
-        DEFAULT, NEG, PLUS, MINUS, MULT, DIV
-    };
-    private final Type type;
+    private final String type;
     
-    public Function(Type type) {
+    public Function(String type) {
         super();
         this.type = type;
-    }
-
-    public Function(String typeString) {
-        super();
-        switch(typeString.toLowerCase()) {
-            case "neg": case "negate": case "negative":
-                type = Type.NEG;
-                break;
-            case "plus": case "sum":
-                type = Type.PLUS;
-                break;
-            case "minus": case "subtract":
-                type = Type.MINUS;
-                break;
-            case "mult": case "multiply":
-                type = Type.MULT;
-                break;
-            case "div": case "divide":
-                type = Type.DIV;
-                break;
-            default:
-                type = Type.DEFAULT;
-                break;
-        }
     }
     
     @Override
     public double evaluate(double var1, double var2) {
-        switch(type) {
-            case NEG:
+        switch(type.toLowerCase()) {
+            case "neg": case "negative":
                 return - children[0].evaluate(var1, var2);
 
-            case PLUS:
-                double sum = children[0].evaluate(var1, var2);
-                for(int i = 1; i < children.length; i++)
-                    sum += children[i].evaluate(var1, var2);
+            case "plus": case "sum":
+                double sum = 0d;
+                for(ParseTree p : children)
+                    sum += p.evaluate(var1, var2);
                 return sum;
 
-            case MINUS:
+            case "minus": case "subtract":
                 return children[0].evaluate(var1, var2) - children[1].evaluate(var1, var2);
 
-            case MULT:
-                double product = children[0].evaluate(var1, var2);
-                for(int i = 1; i < children.length; i++)
-                    product *= children[i].evaluate(var1, var2);
+            case "mult": case "multiply":
+                double product = 1d;
+                for(ParseTree p : children)
+                    product *= p.evaluate(var1, var2);
                 return product;
 
-            case DIV:
+            case "div": case "divide":
                 return children[0].evaluate(var1, var2) / children[1].evaluate(var1, var2);
+
+            case "mod": case "modulus": case "modulo":
+                return children[0].evaluate(var1, var2) % children[1].evaluate(var1, var2);
+
+            case "pow": case "power":
+                return Math.pow(children[0].evaluate(var1, var2), children[1].evaluate(var1, var2));
+
+            case "abs": case "absolute":
+                return Math.abs(children[0].evaluate(var1, var2));
+
+            case "sqrt": case "squareroot":
+                return Math.sqrt(children[0].evaluate(var1, var2));
+
+            case "exp": case "exponential":
+                return Math.exp(children[0].evaluate(var1, var2));
+
+            case "log": case "ln": case "logarithm":
+                return Math.log(children[0].evaluate(var1, var2));
+
+            case "log2":
+                return Math.log(children[0].evaluate(var1, var2)) / 0.69314718056;  // natural logartim of 2
+
+            case "log10":
+                return Math.log10(children[0].evaluate(var1, var2));
+
+            case "sin": case "sine":
+                return Math.sin(children[0].evaluate(var1, var2));
+
+            case "cos": case "cosine":
+                return Math.cos(children[0].evaluate(var1, var2));
+
+            case "tan": case "tangent":
+                return Math.tan(children[0].evaluate(var1, var2));
+
+            case "asin": case "arcsin":
+                return Math.asin(children[0].evaluate(var1, var2));
+
+            case "acos": case "arccos":
+                return Math.acos(children[0].evaluate(var1, var2));
+
+            case "atan": case "arctan":
+                return Math.atan(children[0].evaluate(var1, var2));
+
+            case "atan2":
+                return Math.atan2(children[0].evaluate(var1, var2), children[1].evaluate(var1, var2));
+
+            case "sinh":
+                return Math.sinh(children[0].evaluate(var1, var2));
+
+            case "cosh":
+                return Math.cosh(children[0].evaluate(var1, var2));
+
+            case "tanh":
+                return Math.tanh(children[0].evaluate(var1, var2));
+
+            case "cbrt": case "cuberoot":
+                return Math.cbrt(children[0].evaluate(var1, var2));
+
+            case "ceil": case "ceiling":
+                return Math.ceil(children[0].evaluate(var1, var2));
+
+            case "floor": case "entier": case "int": case "integer":
+                return Math.floor(children[0].evaluate(var1, var2));
+
+            case "expm1":
+                return Math.expm1(children[0].evaluate(var1, var2));
+
+            case "hypot":
+                return Math.hypot(children[0].evaluate(var1, var2), children[1].evaluate(var1, var2));
+
+            case "max": case "maximum":
+                double max = Double.NEGATIVE_INFINITY;
+                for(ParseTree p : children) 
+                    max = Math.max(max, p.evaluate(var1, var2));
+                return max;
+
+            case "min": case "minimum":
+                double min = Double.POSITIVE_INFINITY;
+                for(ParseTree p : children)
+                    min = Math.min(min, p.evaluate(var1, var2));
+                return min;
+
+            case "random":
+                return Math.random();
+
+            case "round":
+                return Math.rint(children[0].evaluate(var1, var2));
+
+            case "sign": case "singum":
+                return Math.signum(children[0].evaluate(var1, var2));
+
+            case "sq": case "square":
+                return Math.pow(children[0].evaluate(var1, var2), 2);
+
+            case "deg": case "degrees": case "todegrees":
+                return Math.toDegrees(children[0].evaluate(var1, var2));
+
+            case "rad": case "radians": case "toradians":
+                return Math.toRadians(children[0].evaluate(var1, var2));
+
+            case "rect": case "between":
+                double x = children[0].evaluate(var1, var2);
+                if(x > children[1].evaluate(var1, var2) && x < children[2].evaluate(var1, var2))
+                    return 1d;
+                return 0d;  // change to undef later?
+
+            case "mag": case "magnitude":
+                sum = 0d;
+                for(ParseTree p : children)
+                    sum += Math.pow(p.evaluate(var1, var2), 2);
+                return Math.sqrt(sum);
+
+            case "avg": case "average":
+                sum = 0d;
+                for(ParseTree p : children)
+                    sum += p.evaluate(var1, var2);
+                return sum / children.length;
+
+
         }
         return Double.NaN;
     }
