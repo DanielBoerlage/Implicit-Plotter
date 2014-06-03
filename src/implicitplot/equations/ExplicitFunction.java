3cecc71daa54165967cb3da9dc7e929208e84c83
DanielBoerlage <daniel.brlg@gmail.com>
2014-06-02 21:52:40 -0700

    added ExplicitFunction for graphing utility

--- src/implicitplot/equations/ExplicitFunction.java
+++ src/implicitplot/equations/ExplicitFunction.java
@@ -1,7 +1,23 @@
+// Copyright (c) 2014 Daniel Boerlage and Philip Xu
+// Distributed under the GNU GPL software license, see the accompanying
+// file LICENSE or http://www.gnu.org/licenses/gpl-3.0.html
+
+package implicitplot.equations;
+
+/**
+ * @author Daniel Boerlage
+ */
 public class ExplicitFunction extends Eqaution {
 	
-	public ExplicitFunction(String functionString) {
-
+	public ExplicitFunction(String funcString) {
+		super(funcString);
 	}
 
+	public double evaluate(double indepVar) {
+		return evaluate(indepVar, 0);
+	}
+
+	public double derrivative(double indepVar) {
+		return derrivative(indepVar, 0);
+	}
 }
