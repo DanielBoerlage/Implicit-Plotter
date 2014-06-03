1b8edca39daef03ba21dc94a1c43cc873150a91e
DanielBoerlage <daniel.brlg@gmail.com>
2014-06-02 20:39:03 -0700

    added copyright statements to all files, as well as pointer to LICENSE

--- src/implicitplot/equations/Variable.java
+++ src/implicitplot/equations/Variable.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2014 Daniel Boerlage and Philip Xu
+// Distributed under the GNU GPL software license, see the accompanying
+// file LICENSE or http://www.gnu.org/licenses/gpl-3.0.html
+
 package implicitplot.equations;
 
 public class Variable extends ParseTree {
